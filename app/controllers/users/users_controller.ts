98d0c588f4bf69dcdc9c4bcb9dc6f5030607349d
Janderson <jandersonsoares50@hotmail.com>
2025-07-14 03:12:50 -0300

    chore: adjust lint

--- app/controllers/users/users_controller.ts
+++ app/controllers/users/users_controller.ts
@@ -1,13 +1,11 @@
-import AuthService from '#services/users/auth.service'
 import type { HttpContext } from '@adonisjs/core/http'
 import { inject } from '@adonisjs/core'
 import User from '#models/user'
 
 @inject()
 export default class UsersController {
-  constructor(private authService: AuthService) {}
 
-  public async handle({ request, response, auth }: HttpContext) {
+  public async handle({ response, auth }: HttpContext) {
     const user = auth.user as User
 
     const users = await User.query().where('customer_id', user.customerId)
